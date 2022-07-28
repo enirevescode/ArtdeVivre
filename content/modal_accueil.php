10eed3f8e1dde7f3d2c99cca74e584f4d2657e78
enireves <enirevescode@gmail.com>
2022-07-28 22:24:37 +0200

    commit first

---
+++ content/modal_accueil.php
@@ -0,0 +1,24 @@
+<div class="modal show" id="myModal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">ATTENTION ATTENTION</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Bonjour !<br>
+         Ceci est un site pour un hôtel fictif !<br>
+         Je suis en train de le développer ! Merci<br>
+
+         httpS est une option payante mensuellement sur mon hébergeur !<br>
+         je ne l'ai pas mis mais je sais le faire et je ne l'ai pas oublié !
+         Merci de votre compréhension.
+
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
